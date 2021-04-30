7db4e6e7d647fe0d25145daf42911e52314e404b
ethan.mcneal <ethan.mcneal@gmail.com>
2021-04-30 13:23:06 -0600

    made projects component, centered text on images

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,10 +8,18 @@ const Home = () => (
     <>
     <h1>Ethan McNeal</h1>
     <div className='image-container'>
+        <div>
+        <h2 style={{position: 'absolute', top: '30%', left: '25%', zIndex:'3', color: 'white'}}>About Me</h2>
         <img src={kayak} />
-        <img src={linkedinpfp}/>
-        <img src={code}/>
-        
+        </div>
+        <div>
+        <h2 style={{position: 'absolute', top: '30%', left: '45%', zIndex:'3', color: 'white'}}>Get in Contact</h2>
+        <img src={linkedinpfp} />
+        </div>
+        <div>
+        <h2 style={{position: 'absolute', top: '30%', left: '66%', zIndex:'3', color: 'white'}}>My Projects</h2>
+        <img src={code} />
+        </div>   
     </div>
     </>
 )
---
+++ client/src/pages/Projects.js
@@ -0,0 +1,16 @@
+const Projects = () => {
+    return(
+        <div>
+            <div>
+                <h2>Streaks</h2>
+            </div>
+            <div>
+                <h2>MUSN</h2>
+            </div>
+            <div>
+                <h2>TBD</h2>
+            </div>
+        </div>
+    )
+}
+export default Projects
