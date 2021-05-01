916d8ea2b8c67689618d340a49a1db86bd7feacc
ethan.mcneal <ethan.mcneal@gmail.com>
2021-05-01 14:59:35 -0600

    about me done

--- client/src/pages/AboutMe.js
+++ client/src/pages/AboutMe.js
@@ -1,24 +1,28 @@
 import { useState } from "react"
+import hawaii from '../images/hawaii.JPG'
+import kayak from '../images/kayak.jpg'
 
 const AboutMe = () => {
 
-    const [showSkills, setShowSkills] = useState(false)
-    const [showHobbies, setShowHobbies] = useState(false)
-    const [showDevelopment, setShowDevelopment] = useState(false)
-
 
     return(
-        <div style={{display: 'flex', justifyContent: 'center'}}>
-            <div>
-                <h1>Skills</h1>
+        <div>
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+                <img src={hawaii} style={{width: '500px', height: '300px', margin: '3em'}}/>
+                    <div style={{margin: '0 3em 3em 0', fontSize: '22px', textAlign: 'center'}}>
+                        <h1 style={{margin: '0'}}>My Stack</h1>
+                        <p>My name is Ethan McNeal, I have recently finished studying full stack development at the University of Utah's full stack web development cohort. Going in I had intentions to become a fully backend developer, using ruby and ruby on rails, but during the course I fell in love with the React frame work and decided that a front end job using react is what I really want to do. I am curious about cloud computing and testing automation as well. but for now I am diligently studying and polishing my React/JavaScript skills in hopes to find a job. </p>
+                    </div>
             </div>
-            <div>
-                <h1>Hobbies</h1>
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+                <div style={{margin: '0 0 3em 3em', fontSize: '22px', textAlign: 'center'}}>
+                    <h1 style={{margin: '0'}}>Hobbies</h1>
+                    <p>
+                        I was raised in the outdoors, my first memories are of me with a fishing rod in my hand, I love the rocky mountain region, Montana and Utah in particular, other than that I play guitar and in my free time I play PC games.
+                    </p>  
+                </div>
+                <img src={kayak} style={{width: '500px', height: '300px', margin: '0 3em'}}/>  
             </div>
-            <div>
-                <h1>Development</h1>
-            </div>
-
         </div>
     )
 }
---
+++ client/src/pages/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return(
+        <div style={{backgroundColor: 'rgb(34,34,34)'}}>
+            <p>8015023712</p>
+        </div>
+    )
+}
+
+export default Footer
