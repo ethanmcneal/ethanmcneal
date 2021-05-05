564f4614e0d6fcb10b79cebad41fd91ffab4d656
ethan.mcneal <ethan.mcneal@gmail.com>
2021-05-05 13:28:20 -0600

    streaks page mobile responsive

--- client/src/pages/Footer.js
+++ client/src/pages/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
 
     var scroll = Scroll.animateScroll;
     return(
-        <div style={{backgroundColor: 'rgb(34,34,34)', color: 'white', margin: '0', height: '100px', display: 'flex', justifyContent: 'space-between'}}>
+        <div style={{backgroundColor: 'rgb(34,34,34)', color: 'white', margin: '0', height: '100px', display: 'flex', justifyContent: 'space-between', width: window.innerWidth}}>
             <div style={{margin: '1em'}}>
                 <a href={'https://www.linkedin.com/in/ethanmcneal/'} target={'_blank'} rel="noreferrer">
             <img src={LinkedInLogo} alt='linked in' style={{width: '25px', height: '25px', margin: '1em .5em'}}/>
@@ -21,11 +21,11 @@ const Footer = () => {
              </div> }
             <div style={{margin: '0'}}>
                 
-                <p>
+                <p style={{margin: '0'}}>
                     Contact Me:
                 </p>
-            <p style={{margin: '0'}}>801-502-3712</p>
-            <p style={{margin: '0'}}>ethan.mcneal@gmail.com</p>
+            <p style={{margin: '0 1.5em'}}>801-502-3712</p>
+            <p style={{margin: '0 1.5em'}}>ethan.mcneal@gmail.com</p>
             </div>
         </div>
     )
--- client/src/pages/Streaks.js
+++ client/src/pages/Streaks.js
@@ -8,12 +8,12 @@ import { Link } from 'react-router-dom'
 const Streaks = () => {
     return(
         <>
-        <div className='main'>
+        <div className='main' style={{width: window.innerWidth}}>
         {/* <h1>Streaks</h1> */}
         <div className='content'>
             {window.innerWidth < 900 && <div>
-            <img src={logo} alt='streaks'/>
-            <div className='content'>
+            <img src={logo} alt='streaks' />
+            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
              <a href='https://github.com/ethanmcneal/streaks' target='_blank' rel="noreferrer">
             <Button variant="Warning">Github</Button>
             </a>
@@ -26,12 +26,12 @@ const Streaks = () => {
             </Link>
             </div>
             </div>}
-            <div className='info-card'>
+            <div className='info-card' style={{width: window.innerWidth < 900 && window.innerWidth}}>
                 <h1>What is Streaks?</h1>
                 Streaks was my final project at the University of Utah's full time web development cohort. My team was assigned this project with a pretty open prompt along the lines of "Create an app where users can compete with their friends in competitions, the losers have to submit proof of punishment and the winners get some kind of reward". Going in we spent a lot of time planning what this would look like, and how it would work. This is what we came up with: A user would register and be prompted to create or join some streaks, upon creating a streak the creator choses when that streak starts, and once it starts it becomes un-joinable. The last man standing is declared the winner, and the losers have to submit media proof of punishment for all to see. Users can also leave comments and update their information  
             </div>
-            <div>
-            <img src={logo} alt='streaks'/>
+            {window.innerWidth > 900 && <div>
+             <img src={logo} alt='streaks'/>
             <div className='content'>
              <a href='https://github.com/ethanmcneal/streaks' target='_blank' rel="noreferrer">
             <Button variant="Warning">Github</Button>
@@ -45,8 +45,8 @@ const Streaks = () => {
             </Link>
             </div>
             {/* <Button>Github #1</Button> */}
-        </div>
-            <div className='info-card'>
+        </div> }
+            <div className='info-card' style={{width: window.innerWidth < 900 && window.innerWidth}}>
                 <h1>My work on Streaks</h1>
                 In Streaks, I worked as a full stack developer among a team of 4, the main parts of the project I worked on are the custom SQL routes in the back end for the joined table connecting users and streaks together, named "user-streaks", I also worked on the front end with this joined table, using the data from it to create the user's dashboard which display's their streaks, and the Streak show page which displays all the users participating in that streak. I also wrote all the logic using the "status" row on the user-streaks joined table, and the "timeframe" row on the streaks table this was how we determined if the streak was upcoming, ongoing, or if a user had quit/won the streak.  
             </div>
